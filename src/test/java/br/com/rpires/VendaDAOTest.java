4cc36ff163a2cec5b18eb8423efac5999a2f5c5c
Seu Nome <seuemail@dominio.com>
2025-09-30 15:28:04 -0300

    commit mod30

---
+++ src/test/java/br/com/rpires/VendaDAOTest.java
@@ -0,0 +1,12 @@
+package br.com.rpires;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class VendaDAOTest {
+
+    @Test
+    public void testSalvarVenda() {
+        assertTrue(true); // Exemplo de teste simples
+    }
+}
